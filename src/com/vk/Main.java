6cf7b23386876e59cc9cd4a195c5d8e8e555a89a
timirovs <timiroffsergey@gmail.com>
2020-03-08 17:38:52 +0400

    lesson1-task1 is done

--- src/com/vk/Main.java
+++ src/com/vk/Main.java
@@ -1,8 +1,39 @@
 package com.vk;
 
+import com.vk.model.MyExeption;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    /**
+     * Метод для генерации NullPointerException
+     */
+    private static void generateNullPointerException() {
+        String name = null;
+        name.length();
+    }
+
+    /**
+     * Метод для генерации ArrayIndexOutOfBoundsException
+     */
+    private static void generateArrayIndexOutOfBoundsException() {
+        int[] superNumbers = new int[3];
+        int number = superNumbers[superNumbers.length];
+    }
+
+    /**
+     * Стартовый метод программы
+     * @throws Exception тип сгенерированного исключения, которое может быть "выброшено" методом
+     */
+
+    public static void main(String[] args) throws MyExeption {
+        try {
+            generateNullPointerException();
+            generateArrayIndexOutOfBoundsException();
+        } catch (NullPointerException e){
+            System.out.println("либо лыжи не едут");
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("либо я дурак");
+        }
+        throw new MyExeption("kva kva");
     }
 }
---
+++ src/com/vk/model/MyExeption.java
@@ -0,0 +1,8 @@
+package com.vk.model;
+
+public class MyExeption extends Exception {
+
+    public MyExeption(String message) {
+        super(message);
+    }
+}
