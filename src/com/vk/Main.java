729dfc279919eb05c519686eeaa95d0efd206c10
timirovs <timiroffsergey@gmail.com>
2020-03-08 18:11:29 +0400

    lesson1-task1 is done

--- src/com/vk/Main.java
+++ src/com/vk/Main.java
@@ -1,39 +1,15 @@
 package com.vk;
 
-import com.vk.model.MyExeption;
-
 public class Main {
+    private static final int AMOUNT_OF_NUMBERS = 10;
 
-    /**
-     * Метод для генерации NullPointerException
-     */
-    private static void generateNullPointerException() {
-        String name = null;
-        name.length();
-    }
-
-    /**
-     * Метод для генерации ArrayIndexOutOfBoundsException
-     */
-    private static void generateArrayIndexOutOfBoundsException() {
-        int[] superNumbers = new int[3];
-        int number = superNumbers[superNumbers.length];
-    }
-
-    /**
-     * Стартовый метод программы
-     * @throws Exception тип сгенерированного исключения, которое может быть "выброшено" методом
-     */
-
-    public static void main(String[] args) throws MyExeption {
-        try {
-            generateNullPointerException();
-            generateArrayIndexOutOfBoundsException();
-        } catch (NullPointerException e){
-            System.out.println("либо лыжи не едут");
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("либо я дурак");
+    public static void main(String[] args) throws IllegalArgumentException {
+        for (int i = 0; i < AMOUNT_OF_NUMBERS; i++) {
+            int randomNumber = (int) (-100 + Math.random() * (100));
+            if (randomNumber < 0) throw new IllegalArgumentException();
+            double squareRootOfRandomNumber = Math.sqrt(randomNumber);
+            int decimalPartOfSquareRoot = (int) squareRootOfRandomNumber;
+            if (decimalPartOfSquareRoot * decimalPartOfSquareRoot == randomNumber) System.out.println(randomNumber);
         }
-        throw new MyExeption("kva kva");
     }
 }
